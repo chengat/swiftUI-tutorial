e6e51a3e59ca1b321311f0d27277a28e5e4ab362
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-01-04 19:18:37 -0500

    init commit

---
+++ BlossomMovie/BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Nikhil Nambiar Chengat on 2026-01-04.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
