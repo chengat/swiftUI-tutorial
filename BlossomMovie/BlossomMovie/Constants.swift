d1538270c2ee528ac81460597285156714fb513c
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-01-04 19:39:58 -0500

    Add Constants struct for home screen strings
    Introduces a Constants struct to centralize string values related to the home screen, such as 'Home' and its icon identifier. This helps maintain consistency and simplifies future updates.

---
+++ BlossomMovie/BlossomMovie/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  BlossomMovie
+//
+//  Created by Nikhil Nambiar Chengat on 2026-01-04.
+//
+
+import Foundation
+
+struct Constants {
+    static let homeString = "Home"
+    
+    
+    static let homeIconString = "house"
+    
+}
