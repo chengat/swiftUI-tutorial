68d0789ea2c6a24d891fbeaf3cd591e7bd7ef35e
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-01-04 19:57:31 -0500

    Add HomeView SwiftUI view
    Introduces a new HomeView struct with a basic SwiftUI view displaying a 'Hello, World!' text. Includes a preview for development.

---
+++ BlossomMovie/BlossomMovie/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  BlossomMovie
+//
+//  Created by Nikhil Nambiar Chengat on 2026-01-04.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
