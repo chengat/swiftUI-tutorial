9a31c04166d947ff238c6fcf751e8854e36c7b1e
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-01-04 22:06:09 -0500

    Update HorizontalListView.swift
    a scrollable list of all pics

--- BlossomMovie/BlossomMovie/HorizontalListView.swift
+++ BlossomMovie/BlossomMovie/HorizontalListView.swift
@@ -9,13 +9,28 @@ import SwiftUI
 
 struct HorizontalListView: View {
     let header = Constants.trendingMovieString // let = final, no change
-    var title = [Constants.testTitleURL, Constants.testTitleURL2, Constants.testTitleURL3] // changeable
+    var titles = [Constants.testTitleURL, Constants.testTitleURL2, Constants.testTitleURL3] // changeable
     
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading){
             Text(header)
                 .font(.title) // Increases the size of the text
+            
+            ScrollView(.horizontal) {
+                LazyHStack { // only loads when in view
+                    ForEach(titles, id:\.self) { title in
+                        AsyncImage(url: URL(string: title)) {image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        
+                    }
+                }
+            }
                 
         }
     }
