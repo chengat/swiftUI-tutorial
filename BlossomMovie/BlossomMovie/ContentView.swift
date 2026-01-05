0c916d1eb8e309edbd1e787bba7af0f940b43434
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-01-04 19:39:40 -0500

    Added a tab view for home

--- BlossomMovie/BlossomMovie/ContentView.swift
+++ BlossomMovie/BlossomMovie/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Home", systemImage: "house") {
+                Text("Home")
+            }
         }
-        .padding()
     }
 }
 
