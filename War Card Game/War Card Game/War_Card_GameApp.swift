cf6c2ba8a8f27947e6593e6f64213bd14679be60
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-02-11 22:35:23 -0500

    new app as per tutorial

---
+++ War Card Game/War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Nikhil Nambiar Chengat on 2026-02-11.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
