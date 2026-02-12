f8eba504f938b9382d86ee1bb8e2df09a6d4234f
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-02-11 19:31:57 -0500

    another project/tutorial for learning swiftUI
    https://www.youtube.com/watch?v=oRc4lLmvHyE&t=4170s

---
+++ learningSwiftUI/learningSwiftUI/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  learningSwiftUI
+//
+//  Created by Nikhil Nambiar Chengat on 2026-02-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.blue)
+                .ignoresSafeArea()
+            VStack (alignment: .leading) {
+               
+                Image("kitty")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    
+                
+                HStack {
+                    Text("Kiki's Kitties!")
+                        .font(Font.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                    VStack{
+                        Text("4 cuties")
+                        HStack {
+                            Image(systemName: "cat.fill")
+                            Image(systemName: "cat.fill")
+                            Image(systemName: "cat.fill")
+                            Image(systemName: "cat.fill").foregroundStyle(.black)
+                        }
+                    }.foregroundStyle(.yellow)
+                        .font(Font.headline)
+                    
+                   
+                        
+                    
+                    
+                }
+                Text("These kitties are so adorable!")
+                
+                
+                
+            }.padding()
+                .background(){
+                    Rectangle()
+                        .foregroundStyle(.white)
+                    
+                }.clipShape(RoundedRectangle(cornerRadius: 16))
+                .padding(20)
+            
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learningSwiftUI/learningSwiftUI/learningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningSwiftUIApp.swift
+//  learningSwiftUI
+//
+//  Created by Nikhil Nambiar Chengat on 2026-02-11.
+//
+
+import SwiftUI
+
+@main
+struct learningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
