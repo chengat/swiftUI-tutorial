69a05617bfb248c1af58055dc5b7cd5c70e5a2af
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-02-11 23:25:47 -0500

    swift basics

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let name: String = "John" // constant, cannot be changed
+var playerScore: Int = 0
+var cpuScore = 0
+
+playerScore += 1
+
+print(playerScore)
