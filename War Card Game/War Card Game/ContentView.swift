8fedfa75274420e1589887fbebacde58ef105f94
Nikhil Nambiar Chengat <nikhilnambiar1212@gmail.com>
2026-02-11 22:59:50 -0500

    coded the skelton using swift UI elements

--- War Card Game/War Card Game/ContentView.swift
+++ War Card Game/War Card Game/ContentView.swift
@@ -10,8 +10,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Image()
-            VStack {
+            Image("background-wood-grain")
+            VStack(spacing: 50){
+                Image("logo")
+                
+                HStack(spacing: 50) {
+                    Image("card2")
+                   
+                    Image("card3")
+                }
+                Image("button")
+                
+                HStack (spacing: 80){
+                    VStack(spacing: 30){
+                        Text("Player")
+                            .font(Font.title2)
+                            .fontWeight(.semibold)
+                        Text("0")
+                            .font(Font.largeTitle.bold())
+                    }
+                    
+                    VStack(spacing: 30){
+                        Text("CPU")
+                            .font(Font.title2)
+                            .fontWeight( .semibold)
+                        Text("0")
+                            .font(Font.largeTitle.bold())
+                    }
+                    
+                }
+                .foregroundColor(Color.white)
+                
                 
             }
             .padding()
